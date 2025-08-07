7a53f3a7d8b9aa990111d41b1945ec2178d1132c
shash64 <34834756+shash64@users.noreply.github.com>
2025-08-07 10:20:38 -0400

    Add base version

--- Core/fonctions.py
+++ Core/fonctions.py
@@ -1,9 +1,32 @@
-import configparser
 import requests
 import socket
-import time
 import os
+import json
+from datetime import datetime
 
+# =============================================================================
+# FONCTION LOG.TXT
+# =============================================================================
+
+def log_message(message):
+    try:
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        log_path = os.path.join(base_dir, "..", "data", "log.txt")
+        
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = f"[{timestamp}] {message}\n"
+        
+        with open(log_path, 'a', encoding='utf-8') as log_file:
+            log_file.write(log_entry)
+            
+    except Exception as e:
+        print("Impossible d'écrire dans le fichier log : {e}")
+        print(f"Message original: {message}")
+
+
+# =============================================================================
+# FONCTIONS UTILITAIRES
+# =============================================================================
 
 # Obtenir l'adresse IP locale de la machine
 def get_ip():
@@ -13,47 +36,58 @@ def get_ip():
         ip = s.getsockname()[0]
         s.close()
         return ip
+    
     except Exception as e:
-        return f"Erreur : {e}"
-    
-    
-# Charger la configuration depuis le fichier config.ini    
+        log_message(f"[FONCTION]: Erreur lors de la récupération de l'adresse IP: {e}")
+        return None
+      
+# Charger la configuration depuis le fichier config.json    
 def charger_config():
-    config = configparser.ConfigParser()
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(base_dir, "..", "config.ini")  
-    config.read(config_path)
+    config_path = os.path.join(base_dir, "..", "data", "config.json")
+    
+    if not os.path.exists(config_path):
+        log_message(f"[FONCTION]: Le fichier de configuration est introuvable à {config_path}")
+        return None
+    else: 
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+    return config
 
-    url_serveur = config.get("Serveur", "url")
-    vitesse = config.getint("Serveur", "vitesse_boucle")
 
-    return url_serveur, vitesse
-
+# =============================================================================
+# FONCTIONS D'IMPRESSION
+# =============================================================================
 
 # Télécharger un fichier PDF depuis le serveur
-def telecharger_fichier():
-    url_serveur, vitesse = charger_config()
-    ip = get_ip()
-    url = f"{url_serveur}{ip}.pdf"
-
-    # Créer un répertoire pour stocker les fichiers téléchargés
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    fichiers_dir = os.path.join(base_dir, "fichiers")
-    os.makedirs(fichiers_dir, exist_ok=True)
-    fichier_path = os.path.join(fichiers_dir, f"{ip}.pdf")
-
+def telecharger_fichier(url_serveur, ip, type_fichier):
+    extension = 'pdf' if type_fichier == 'PDF' else 'txt'
+    prefixs = ["print_","ticket_"]
+    nom_fichier = f"{ip}.{extension}"
     try:
-        print(f"Téléchargement du fichier depuis {url}")
-        response = requests.get(url, timeout = vitesse)
-        with open(fichier_path, 'wb') as f:
-            f.write(response.content)
-        print("Téléchargement réussi")
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        fichiers_dir = os.path.join(base_dir, "..", "data", "fichiers")
+        os.makedirs(fichiers_dir, exist_ok=True)
 
-    except requests.exceptions.RequestException as e:
-        print(f"Échec du téléchargement : {e}")
-        return False
-    
+        for prefix in prefixs:
+            nom_fichier_final = f"{prefix}{nom_fichier}"
+            url_complete = f"{url_serveur}{nom_fichier_final}"
 
+            try:
+                requete = requests.head(url_complete)
 
+                if requete.status_code == 200:
+                    reponse = requests.get(url_complete)
+                    chemin_destination = os.path.join(fichiers_dir, nom_fichier_final)
+                    with open(chemin_destination, 'wb') as f:
+                        f.write(reponse.content)
+                    return chemin_destination
 
+            except requests.exceptions.RequestException:
+                log_message(f"[FONCTION]: Erreur lors de la requête {url_complete}: {e}")
+                continue
+        return None
 
+    except Exception as e:
+        log_message(f"[FONCTION]: Erreur inattendue dans la fonction de téléchargement: {e}")
+        return None
---
+++ Core/serveur.py
@@ -0,0 +1,64 @@
+import time
+import os
+from fonctions import charger_config, get_ip, telecharger_fichier, log_message
+
+def main():
+    print("Serveur d'impression en cours d'exécution...")
+
+    auto_ip = get_ip()
+
+    try:
+        while True:
+            config = charger_config()
+            if not config:
+                log_message("[SERVEUR]: Configuration introuvable ou non définie, nouvel essai dans 10s...")
+                time.sleep(10)
+                continue
+
+            serveur_config = config.get('Serveur', {})
+            url_serveur = serveur_config.get('url')
+            vitesse = serveur_config.get('vitesse_boucle', 1000)
+
+            if not url_serveur:
+                log_message("[SERVEUR]: L'URL du serveur n'est pas définie dans config.json")
+                time.sleep(5)
+                continue
+
+            #Chaque section d'imprimante
+            for i in range(1, 6):
+                nom_imprimante = f'Imprimante{i}'
+                imprimante_config = config.get(nom_imprimante, {})
+                
+                type_fichier = imprimante_config.get('type', 'Desactiver')
+                if type_fichier == 'Desactiver':
+                    continue
+
+                ip = None
+                mode_source = imprimante_config.get('source_mode', 'Auto IP')
+
+                if mode_source == 'Auto IP':
+                    ip = auto_ip
+
+                elif mode_source == 'Manuel':
+                    ip = imprimante_config.get('source_ip_manuelle')
+                
+                if not ip:
+                    log_message(f"[SERVEUR]: IP non définie pour {nom_imprimante}")
+                    continue
+
+                fichier_telecharge = telecharger_fichier(url_serveur, ip, type_fichier)
+
+                if fichier_telecharge:
+                    pass
+                    # La logique d'impression et de suppression sera ajoutée ici
+            
+            delai_secondes = vitesse/ 1000
+            time.sleep(delai_secondes)
+
+    except Exception as e:
+        log_message(f"[SERVEUR]: Erreur dans la boucle principale: {e}")
+    finally:
+        print("--- Fin du processus serveur ---")
+
+if __name__ == "__main__":
+    main()
